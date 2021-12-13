edf139eb67fe21c6ecb91e5cc48ed6b6e0755df5
emazack <em.zaccaria@gmail.com>
2021-12-13 23:53:09 +0100

    insert first files and data

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("PALLE");
+
+// creo campo da gioco
+    // all'interno del campo saranno generate le palle
+// creo tabellone punteggio che parte da zero
+    // il campo si incrementa al click di una palla
+// creo pulsante reset punteggio
+    //clicco il pulsante e il punteggio torna a zero
+// creo pulsante inizio partita
+    // Clicco un pulsante e parte la partita
+        // genero delle palle
+            // una ogni 0,5 secondi viene generata
+            // con raggio differente
+            // con colore randomico
+            // con velocità randomica
+        // le palle vengono generate in un campo di palle definito
+        // al click della palla viene incrementato di uno il punto
+// creo tabellone countdown
+    // alla fine del count down il campo da gioco si svuota
+
+
+
